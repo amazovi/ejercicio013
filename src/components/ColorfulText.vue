c6f3fe5584e67b6bd1128f64319feac23025da72
curso14 <curso14@nomail.com>
2024-08-19 11:12:27 +0200

    Proyecto vue primer commit faltan test

---
+++ src/components/ColorfulText.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="colorful-text" @mouseover="isHovered = true" @mouseleave="isHovered = false">
+    <p :style="{ color: isHovered ? 'red' : 'black' }">¡Pasa el cursor sobre este texto!</p>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+
+const isHovered = ref(false)
+</script>
+
+<style scoped>
+.colorful-text {
+  text-align: center;
+  margin-top: 20px;
+  font-size: 20px;
+  cursor: pointer;
+}
+</style>
---
+++ src/components/FavoriteAnimal.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="favorite-animal">
+    <h2>Question: What is your favorite animal?</h2>
+    <input v-model="animal" placeholder="Type your favorite animal" />
+    <button @click="showAnswer = !showAnswer">{{ showAnswer ? 'Hide' : 'Show' }} Answer</button>
+    <p v-if="showAnswer">Your favorite animal is a {{ animal }}!</p>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+
+const showAnswer = ref(false)
+const animal = ref('')
+</script>
+
+<style scoped>
+.favorite-animal {
+  text-align: center;
+  margin: 20px;
+}
+
+.favorite-animal input {
+  margin-top: 10px;
+  padding: 5px;
+  width: 200px;
+}
+
+.favorite-animal button {
+  margin-top: 10px;
+  padding: 5px 10px;
+  cursor: pointer;
+}
+
+.favorite-animal p {
+  margin-top: 10px;
+  font-weight: bold;
+}
+</style>
